8a7f426570369a891405b047923e6859a37c1c76
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2022-08-26 08:23:36 +0200

    Added Unit Testing with JEST

---
+++ ex4.2/README.md
@@ -0,0 +1,1 @@
+# Enhetstestning med JEST
---
+++ ex4.2/src/App.jsx
@@ -0,0 +1,12 @@
+import './app.css';
+
+// Det gamla sättet att skapa komponenter...
+const App = () => {
+  return (
+    <section className='container'>
+      <h1 className='title'>Test Driven Development with Jest</h1>
+    </section>
+  );
+};
+
+export default App;
